dc4ba9821c487ff2fb2f9d0e911505765f09b8ea
AKSHAYABHANG <akshayabhang43@gmail.com>
2018-01-15 16:20:16 +0530

    Adding Unit Tests

--- SolrQuery/activity.go
+++ SolrQuery/activity.go
@@ -2,6 +2,7 @@ package SolrQuery
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/TIBCOSoftware/flogo-lib/core/activity"
 	"github.com/vanng822/go-solr/solr"
@@ -17,7 +18,6 @@ const (
 	ivQuery             = "Query"
 	ivFilterQuery       = "FilterQuery"
 	ivSort              = "Sort"
-	ivQueryParams       = "queryParams"
 	ivStart             = "Start"
 	ivRow               = "Row"
 	ivFieldList         = "FieldList"
@@ -48,26 +48,55 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 	// do eval
 	host := context.GetInput(ivHost).(string)
 	port := context.GetInput(ivPort).(int)
-	// username := context.GetInput(ivUserName).(string)
-	// password := context.GetInput(ivPassword).(string)
+	username := context.GetInput(ivUserName).(string)
+	password := context.GetInput(ivPassword).(string)
 	collection := context.GetInput(ivCollection).(string)
 
 	query := context.GetInput(ivQuery).(string)
-	// filterquery := context.GetInput(ivFilterQuery).(string)
-	// sort := context.GetInput(ivSort).(string)
-	// queryparams := context.GetInput(ivQueryParams).(string)
-	// fieldlist := context.GetInput(ivFieldList).(string)
-	// defaultsearchlist := context.GetInput(ivDefaultSearchList).(string)
-	// rawqueryparameter := context.GetInput(ivRawQueryParameter).(string)
-	// start := context.GetInput(ivStart).(string)
-	// row := context.GetInput(ivRow).(string)
+	filterquery := context.GetInput(ivFilterQuery).(string)
+	sort := context.GetInput(ivSort).(string)
+	fieldlist := context.GetInput(ivFieldList).(string)
+	//defaultsearchlist := context.GetInput(ivDefaultSearchList).(string)
+	rawqueryparameter := context.GetInput(ivRawQueryParameter).(string)
+	start := context.GetInput(ivStart).(int)
+	row := context.GetInput(ivRow).(int)
+
 	LocalURL := fmt.Sprintf("http://%s:%d/solr", host, port)
 
 	si, _ := solr.NewSolrInterface(LocalURL, collection)
+	if username != "" && password != "" {
+		si.SetBasicAuth(username, password)
+
+	}
 
 	// Query/Search Solr
 	solrquery := solr.NewQuery()
 	solrquery.Q(query)
+
+	if filterquery != "" {
+		solrquery.FilterQuery(filterquery)
+
+	}
+	if sort != "" {
+		solrquery.Sort(sort)
+
+	}
+
+	if fieldlist != "" {
+		solrquery.FieldList(fieldlist)
+
+	}
+	solrquery.Start(start)
+	solrquery.Rows(row)
+
+	if rawqueryparameter != "" {
+		params := strings.Split(rawqueryparameter, "&")
+		for i := 0; i < len(params); i++ {
+			keyval := strings.Split(params[i], "=")
+			solrquery.AddParam(keyval[0], keyval[1])
+		}
+	}
+
 	s := si.Search(solrquery)
 	r, _ := s.Result(nil)
 	fmt.Println(r.Results.Docs)
